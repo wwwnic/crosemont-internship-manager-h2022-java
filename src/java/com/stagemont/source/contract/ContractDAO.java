50f8843a9228cfa96fa126b3812cab69f88d431b
Melissa Bilhete <1756253@crosemont.qc.ca>
2022-04-09 22:26:42 -0400

    Ajout de la page Entreprise liste des candidatures

---
+++ src/java/com/stagemont/source/contract/ContractDAO.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.stagemont.source.contract;
+
+import com.stagemont.entities.Contract;
+import java.util.List;
+
+/**
+ *
+ * @author melis
+ */
+public class ContractDAO implements ContractSource {
+
+    @Override
+    public List<Contract> getAllContract() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
