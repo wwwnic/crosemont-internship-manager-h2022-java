e2e751451127e4629bfb85e0062b6496f9f81ca2
Nicolas Brunet <1816098@crosemont.qc.ca>
2022-05-16 18:43:20 -0400

    Ajout de la version teacher du dashboard etudiant

--- src/java/com/stagemont/source/contract/ContractFakeData.java
+++ src/java/com/stagemont/source/contract/ContractFakeData.java
@@ -55,6 +55,11 @@ public class ContractFakeData implements ContractSource{
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
+    @Override
+    public List<Contract> getContractByStudentId(int student_id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
     
     
 }
