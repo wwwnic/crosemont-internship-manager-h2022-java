436d6f3ac6ca70edd92e9c31af44db5afc0eb5c4
Nicolas Brunet <1816098@crosemont.qc.ca>
2022-05-25 22:43:01 -0400

    Permet d'afficher dashboard contrat pour tlm

--- src/java/com/stagemont/controller/action/edit/delete/ContractDelete.java
+++ src/java/com/stagemont/controller/action/edit/delete/ContractDelete.java
@@ -3,9 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.stagemont.controller.action.company;
+package com.stagemont.controller.action.edit.delete;
 
 import com.stagemont.controller.actionsHelper.AbstractAction;
+import com.stagemont.entities.Contract;
 import com.stagemont.entities.Student;
 import com.stagemont.source.contract.ContractDAO;
 import com.stagemont.source.contract.ContractSource;
@@ -19,45 +20,43 @@ import javax.servlet.http.Cookie;
  * @author melis
  */
 public class ContractDelete extends AbstractAction {
-    
-    
-    private static ContractSource DATA_CONTRACT_DAO = new ContractDAO();
-    
-    private final ContractSource DATA_CONTRACTS_DAO = new ContractDAO();
-    
-    private static StudentSource DATA_STUDENT_DAO  = new StudentDAO();
-    
 
+    private static ContractSource DATA_CONTRACT_DAO = new ContractDAO();
+
+    private final ContractSource DATA_CONTRACTS_DAO = new ContractDAO();
+
+    private static StudentSource DATA_STUDENT_DAO = new StudentDAO();
 
     @Override
     public String execute() {
-        
+
         int id = Integer.parseInt(request.getParameter("idC"));
-        
+        Contract contract = DATA_CONTRACT_DAO.getContractById(id);
         DATA_CONTRACT_DAO.deleteContract(id);
-        
+
         //--------------------
-        
         Cookie[] idCookie = request.getCookies();
-        int idConnecte=-1;
-        if (idCookie!=null) {
-            for (int i=0; i<idCookie.length; i++) {
-                if ((idCookie[i].getName()).equals("idConnecte")){
+        int idConnecte = -1;
+        if (idCookie != null) {
+            for (int i = 0; i < idCookie.length; i++) {
+                if ((idCookie[i].getName()).equals("idConnecte")) {
                     idConnecte = Integer.parseInt(idCookie[i].getValue());
                 }
             }
         }
         request.setAttribute("idConnecte", idConnecte);
-        
+
         List<com.stagemont.entities.Contract> listContracts = DATA_CONTRACTS_DAO.getContractByComapnyId(idConnecte);
         request.setAttribute("listContracts", listContracts);
-        
+
         List<Student> listStudents = DATA_STUDENT_DAO.getAllStudents();
         request.setAttribute("listStudents", listStudents);
-        
+
         String userType = request.getSession(false).getAttribute("type").toString();
-        return userType + "/contractList";
+        if (userType.equals("company")) {
+            return userType + "/contractList";
+        }
+        return "~showStudentDashboard?id=" + contract.getStudent_id();
     }
 
-    
 }
